62416f784841854a5031956d17193d7042097f66
nagesh <rishichaubey1426@gmail.com>
2024-12-01 01:20:42 +0530

    image field in data base

--- pages/models.py
+++ pages/models.py
@@ -76,12 +76,13 @@ class Menu(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     menu_details = models.TextField(null=True, blank=True)
     price = models.IntegerField(null=True, blank=True)
+    image = models.ImageField(upload_to='menu_images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.menu_name)
+            self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
 
